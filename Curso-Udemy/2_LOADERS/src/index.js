1a387646b3c9789a76ffbedc6717e457d2e3c7ea
Jmar33 <josimarferreira922@yahoo.com.br>
2024-05-29 07:25:38 -0300

    feature cria loader para arquivos e funcionalidades JS

--- Curso-Udemy/2_LOADERS/src/index.js
+++ Curso-Udemy/2_LOADERS/src/index.js
@@ -13,3 +13,11 @@ image.insertBobImage();
 const button = new Button();
 
 button.crete();
+
+const obj = {a: 1, b: 2, c: 3, d:4, e:99};
+
+let {a, b, ...teste} = obj;
+
+console.log(a);
+console.log(b);
+console.log(teste);
