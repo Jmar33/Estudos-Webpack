f13012e64fd4204db1a126751cb9ef15717bccfd
Jmar33 <josimarferreira922@yahoo.com.br>
2024-05-31 07:18:52 -0300

    [feature] Adiciona integração ao font-awesome

--- Curso-Udemy/5_INTEGRACOES/src/index.js
+++ Curso-Udemy/5_INTEGRACOES/src/index.js
@@ -4,6 +4,15 @@ import './fonts.css'
 // Import Jquery
 import $ from 'jquery';
 
+//Import font awesome
+import {library, dom} from '@fortawesome/fontawesome-svg-core'
+import { faBacon, faAnchor } from '@fortawesome/free-solid-svg-icons';
+
+library.add(faBacon);
+library.add(faAnchor);
+
+dom.watch(); //Substitui nossa fonte pelo respectivo ícone
+
 const body = $('body');
 
 const p = $('<p></p>').text('Inserindo texto').css('color', 'red');
