77ded55874c6391ec0817ddbc23b2a16c2d23a22
Jmar33 <josimarferreira922@yahoo.com.br>
2024-06-01 06:35:16 -0300

    [feature] Cria integração com o React

---
+++ Curso-Udemy/7_INTEGRACAO_REACT/src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+class App extends React.Component{
+  render(){
+    return(
+      <div>
+        <h1>Meu projeto em React e Webpack</h1>
+        <p>Integrado com sucesso!</p>
+      </div>
+    )
+  }
+}
+
+export default App
