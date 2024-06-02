e6224f21ce2b158dbbe0648c3a27184844ed5f79
Jmar33 <josimarferreira922@yahoo.com.br>
2024-06-02 08:07:15 -0300

    [feature] Adiciona recursos de shiming

---
+++ Curso-Udemy/11_RECURSOS_WEBPACK/src/index.js
@@ -0,0 +1,5 @@
+//Shiming
+//Com o shiming deixamos como responsabilidade do webpack definir quem são nossas variáveis globais
+console.log(_.difference([1, 2, 3], [4, 5])); 
+
+console.log(_.lastIndexOf([1, 2, 3], 2));
