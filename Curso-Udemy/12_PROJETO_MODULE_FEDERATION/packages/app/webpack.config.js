87e531485577db163a65add074ca54809ccbcf49
Jmar33 <josimarferreira922@yahoo.com.br>
2024-06-02 09:37:03 -0300

    [feature] Cria estrutura inicial do projeto utlizando module federation

---
+++ Curso-Udemy/12_PROJETO_MODULE_FEDERATION/packages/app/webpack.config.js
@@ -0,0 +1,42 @@
+const path = require('path');
+const HTMLWebpackPlugin = require('html-webpack-plugin');
+const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin');
+const { devServer } = require('../../../11_RECURSOS_WEBPACK/webpack.config');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    publicPath: 'http://localhost:9001/'
+  },
+  devServer: {
+    historyFallbackApi: true,
+    port: 9001
+  }, 
+  resolve: {
+    extensions: ['.jsx', '.js', '.json']
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        use: require.resolve('babel-loader'),
+        options: {
+          presets: [require.resolve('@babel/preset-react')]
+        }
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      }
+    ]
+  },
+  plugins: [
+    new HTMLWebpackPlugin({
+      filename: 'index.html',
+      template: './public/index.html',
+      title: 'App'
+    })
+  ]
+}
