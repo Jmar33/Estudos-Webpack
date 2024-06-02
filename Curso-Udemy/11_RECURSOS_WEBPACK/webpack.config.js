1465cb2cd706a54bc452966656f5f7884fd800c0
Jmar33 <josimarferreira922@yahoo.com.br>
2024-06-02 08:23:55 -0300

    [feature] Melhora o tempo de build do webpack

--- Curso-Udemy/11_RECURSOS_WEBPACK/webpack.config.js
+++ Curso-Udemy/11_RECURSOS_WEBPACK/webpack.config.js
@@ -1,15 +1,34 @@
 const path = require('path');
 const webpack = require('webpack');
+const HTMLWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   entry: './src/index.js',
   output: {
-    filename: 'bundle.js',
+    filename: '[name].js',
     path: path.resolve(__dirname, 'dist')
+  },
+  devServer: {
+    historyApiFallback: true
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        include: path.resolve(__dirname, 'src')
+      }
+    ]
   },
   plugins: [
     new webpack.ProvidePlugin({
       _: 'lodash'
+    }),
+    new HTMLWebpackPlugin({
+      title: 'Recursos Webpack'
     })
-  ]
+  ],
+  optimization: {
+    runtimeChunk: true
+  }
 }
